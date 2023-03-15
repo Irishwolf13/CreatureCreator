94dc541622e040d7348ce9655481dda8ea1e7974
John Rooney <rooneyjohn@gmail.com>
2023-03-15 17:47:06 -0400

    installed react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from "react-dnd-html5-backend";
+
 import DragDrop from './components/DragDrop';
 
 function App() {
   return (
-    <DndProvider backend={ HTML5Backend }>
-      <div className="App">
-        <DragDrop />
-      </div>
-    </DndProvider>
+    <div className="App">
+      <DndProvider backend={ HTML5Backend }>
+        <Routes>
+          <Route 
+            path="/" 
+            element={<DragDrop />}
+          />
+        </Routes>
+      </DndProvider>
+    </div>
   );
 }
 
